6bcbabd0c678684928d0cb72e21cf7854108a931
Your Name <your-email@example.com>
2024-11-16 00:28:41 +0600

    add some changes

---
+++ src/components/Login.jsx
@@ -0,0 +1,53 @@
+import { Link } from "react-router-dom";
+
+const Login = () => {
+  return (
+    <div className="min-h-screen flex justify-center items-center">
+      <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+        <h2 className="text-gray-400 text-2xl font-medium text-center py-4">
+          Log in Your account
+        </h2>
+        <form className="card-body">
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Email</span>
+            </label>
+            <input
+              type="email"
+              placeholder="email"
+              className="input input-bordered"
+              required
+            />
+          </div>
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Password</span>
+            </label>
+            <input
+              type="password"
+              placeholder="password"
+              className="input input-bordered"
+              required
+            />
+            <label className="label">
+              <a href="#" className="label-text-alt link link-hover">
+                Forgot password?
+              </a>
+            </label>
+          </div>
+          <div className="form-control mt-6">
+            <button className="btn btn-primary">Login</button>
+          </div>
+        </form>
+        <p className="text-center font-semibold py-3">
+          Dont have account?{" "}
+          <Link className="text-red-500" to="/auth/register">
+            Register
+          </Link>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
